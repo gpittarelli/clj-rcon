4cd25755fcc1439680f63a6a1a4af47da8b4233d
George Pittarelli <g@gjp.cc>
2016-05-20 22:03:37 -0700

    add manifold dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[aleph "0.4.1"]
+                 [manifold "0.1.4"]
                  [smee/binary "0.5.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]
                    :global-vars {*warn-on-reflection* true}}}
